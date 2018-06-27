42cc49a8562f3e357e33dbc5d3086fa0c7c99a4c
friedkeenan <friedkeenan@gmail.com>
2018-06-26 23:11:01 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+user="username"
+pw="password"
+server_ip="play.avatarmc.com"
+server_version="1.9.2"
+admin="admin"
+vote_wait=3*24*60*60 #Time between vote beginning and ending in seconds
+vote_die=4*24*60*60 #Time between vote ending and vote being gone forever and ever amen
